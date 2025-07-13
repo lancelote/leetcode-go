c31c5811d60a0d35a61da866d9559916a46eca5e
Pavel Karateev <lancelote.du.lac@gmail.com>
2025-07-13 13:33:30 +0200

    add p104 solution

---
+++ src/p104/p104.go
@@ -0,0 +1,20 @@
+package p104
+
+import (
+	. "github.com/lancelote/leetcode-go/utils/btree"
+)
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	return 1 + max(maxDepth(root.Left), maxDepth(root.Right))
+}
---
+++ src/p104/p104_test.go
@@ -0,0 +1,36 @@
+package p104
+
+import (
+	"github.com/lancelote/leetcode-go/utils/btree"
+	"testing"
+)
+
+func Test_maxDepth(t *testing.T) {
+	tests := []struct {
+		name  string
+		slice []any
+		want  int
+	}{
+		{
+			"large example",
+			[]any{3, 9, 20, nil, nil, 15, 7},
+			3,
+		},
+		{
+			"small example",
+			[]any{1, nil, 2},
+			2,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			root := btree.NewTreeFromSlice(tt.slice)
+			got := maxDepth(root)
+
+			if got != tt.want {
+				t.Errorf("got=%d, want=%d", got, tt.want)
+			}
+		})
+	}
+}
