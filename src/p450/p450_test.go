5cd12ed0a604b9e6e35648603c5927d6bfb2e751
Pavel Karateev <lancelote.du.lac@gmail.com>
2025-07-16 22:16:21 +0200

    add p450 solution

---
+++ src/p450/p450_test.go
@@ -0,0 +1,41 @@
+package p450
+
+import (
+	"github.com/google/go-cmp/cmp"
+	"github.com/lancelote/leetcode-go/utils/btree"
+	"testing"
+)
+
+func Test_deleteNode(t *testing.T) {
+	tests := []struct {
+		name      string
+		inSlice   []any
+		key       int
+		wantSlice []any
+	}{
+		{
+			"key exist",
+			[]any{5, 3, 6, 2, 4, nil, 7},
+			3,
+			[]any{5, 4, 6, 2, nil, nil, 7},
+		},
+		{
+			"key doesn't exit",
+			[]any{5, 3, 6, 2, 4, nil, 7},
+			0,
+			[]any{5, 3, 6, 2, 4, nil, 7},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			root := btree.NewTreeFromSlice(tt.inSlice)
+			got := deleteNode(root, tt.key)
+			gotSlice := btree.SliceFromTree(got)
+
+			if diff := cmp.Diff(tt.wantSlice, gotSlice); diff != "" {
+				t.Error(diff)
+			}
+		})
+	}
+}
