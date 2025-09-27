af29bbb6f48c7516edcc056de301e1e4f117f144
Pavel Karateev <lancelote.du.lac@gmail.com>
2025-09-27 17:50:18 +0200

    top-down p72 solution

--- src/p72/p72.go
+++ src/p72/p72.go
@@ -12,6 +12,14 @@ func min3(a, b, c int) int {
 }
 
 func minDistance(word1 string, word2 string) int {
+	memo := make([][]int, len(word1))
+	for i := range memo {
+		memo[i] = make([]int, len(word2))
+		for j := range memo[i] {
+			memo[i][j] = -1
+		}
+	}
+
 	var req func(int, int) int
 	req = func(i1 int, i2 int) int {
 		if i1 < 0 {
@@ -22,15 +30,22 @@ func minDistance(word1 string, word2 string) int {
 			return i1 + 1
 		}
 
-		if word1[i1] == word2[i2] {
-			return req(i1-1, i2-1)
+		if memo[i1][i2] != -1 {
+			return memo[i1][i2]
 		}
 
-		insert := req(i1, i2-1)
-		delete := req(i1-1, i2)
-		replace := req(i1-1, i2-1)
+		var minDist int
+		if word1[i1] == word2[i2] {
+			minDist = req(i1-1, i2-1)
+		} else {
+			insert := req(i1, i2-1)
+			delete := req(i1-1, i2)
+			replace := req(i1-1, i2-1)
+			minDist = min3(insert, delete, replace) + 1
+		}
 
-		return min3(insert, delete, replace) + 1
+		memo[i1][i2] = minDist
+		return minDist
 	}
 
 	return req(len(word1)-1, len(word2)-1)
