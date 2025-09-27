7581c36ce00552b6e5379c0299ee837251dba2d2
Pavel Karateev <lancelote.du.lac@gmail.com>
2025-09-27 21:23:48 +0200

    bottom-up solution for p72

--- src/p72/p72.go
+++ src/p72/p72.go
@@ -12,41 +12,29 @@ func min3(a, b, c int) int {
 }
 
 func minDistance(word1 string, word2 string) int {
-	memo := make([][]int, len(word1))
-	for i := range memo {
-		memo[i] = make([]int, len(word2))
-		for j := range memo[i] {
-			memo[i][j] = -1
-		}
+	dp := make([][]int, len(word1)+1)
+	for i := range dp {
+		dp[i] = make([]int, len(word2)+1)
+	}
+	for i := 1; i <= len(word2); i++ {
+		dp[0][i] = i
+	}
+	for i := 1; i <= len(word1); i++ {
+		dp[i][0] = i
 	}
 
-	var req func(int, int) int
-	req = func(i1 int, i2 int) int {
-		if i1 < 0 {
-			return i2 + 1
+	for i1 := 1; i1 <= len(word1); i1++ {
+		for i2 := 1; i2 <= len(word2); i2++ {
+			if word1[i1-1] == word2[i2-1] {
+				dp[i1][i2] = dp[i1-1][i2-1]
+			} else {
+				dp[i1][i2] = min3(
+					dp[i1-1][i2-1],
+					dp[i1-1][i2],
+					dp[i1][i2-1],
+				) + 1
+			}
 		}
-
-		if i2 < 0 {
-			return i1 + 1
-		}
-
-		if memo[i1][i2] != -1 {
-			return memo[i1][i2]
-		}
-
-		var minDist int
-		if word1[i1] == word2[i2] {
-			minDist = req(i1-1, i2-1)
-		} else {
-			insert := req(i1, i2-1)
-			delete := req(i1-1, i2)
-			replace := req(i1-1, i2-1)
-			minDist = min3(insert, delete, replace) + 1
-		}
-
-		memo[i1][i2] = minDist
-		return minDist
 	}
-
-	return req(len(word1)-1, len(word2)-1)
+	return dp[len(word1)][len(word2)]
 }
