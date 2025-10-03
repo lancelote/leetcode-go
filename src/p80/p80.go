571554057211658e074d6809726f206d5c024007
Pavel Karateev <lancelote.du.lac@gmail.com>
2025-10-03 11:37:55 +0200

    p80 optimize solution

--- src/p80/p80.go
+++ src/p80/p80.go
@@ -1,19 +1,16 @@
 package p80
 
 func removeDuplicates(nums []int) int {
-	if len(nums) < 2 {
+	if len(nums) <= 2 {
 		return len(nums)
 	}
 
-	a, b := nums[0], nums[1]
 	write := 2
-
 	for read := 2; read < len(nums); read++ {
-		if !(a == b && b == nums[read]) {
+		if nums[read] != nums[write-2] {
 			nums[write] = nums[read]
-			write += 1
+			write++
 		}
-		a, b = b, nums[read]
 	}
 
 	return write
