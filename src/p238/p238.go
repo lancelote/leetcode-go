c7fbcde671629715f6dbc84dd690904adb9a855f
Pavel Karateev <lancelote.du.lac@gmail.com>
2025-12-24 14:03:24 +0100

    p238 re-solve

--- src/p238/p238.go
+++ src/p238/p238.go
@@ -2,23 +2,20 @@ package p238
 
 func productExceptSelf(nums []int) []int {
 	r := make([]int, len(nums))
-
-	for i := 0; i < len(r); i++ {
+	for i := range r {
 		r[i] = 1
 	}
 
-	total := nums[0]
-
+	m := nums[0]
 	for i := 1; i < len(nums); i++ {
-		r[i] = total
-		total *= nums[i]
+		r[i] *= m
+		m *= nums[i]
 	}
 
-	total = nums[len(nums)-1]
-
+	m = nums[len(nums)-1]
 	for i := len(nums) - 2; i >= 0; i-- {
-		r[i] *= total
-		total *= nums[i]
+		r[i] *= m
+		m *= nums[i]
 	}
 
 	return r
